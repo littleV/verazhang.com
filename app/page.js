1f3259a4ad598ee4e9f2330ca588baee7aac9094
littleV <wei.vera.zhang@gmail.com>
2025-04-21 08:47:45 +0800

    home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,32 @@
 "use client";
 
 import { useLanguage } from "@/contexts/LanguageContext";
+import Slides from "@/components/slides";
+
+const slides = [
+  {
+    image: "/art/love-story-one.jpg",
+    name: "First Sight",
+  },
+  {
+    image: "/art/love-story-two.jpg",
+    name: "In Love",
+  },
+  {
+    image: "/art/love-story-three.jpg",
+    name: "The Wall",
+  },
+];
 
 export default function Home() {
   const { language } = useLanguage();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {language === "English" ? (
-          <div>Hello World</div>
-        ) : (
-          <div>你好，世界</div>
-        )}
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <div className="text-3xl ">
+          {language === "English" ? "The Love Story" : "爱情故事"}
+        </div>
+        <Slides slides={slides} width="80vw" height="70vh" />
       </main>
     </div>
   );
